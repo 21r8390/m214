174d2441a5af3d28d9d97c4d898931f5296213c0
21r8390 <76058025+21r8390@users.noreply.github.com>
2021-12-19 15:15:01 +0100

    App angepasst mit Passwort byte[]

--- hashing/src/App.java
+++ hashing/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        byte[] passwort = "Sicher123!".getBytes();
+        
         Hasher hasher = new Hasher();
     }
 }
