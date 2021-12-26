177fce58ac1d0cd959d7b2d881cd22b353505338
21r8390 <21r8390@bztf.ch>
2021-12-26 07:04:13 +0100

    Schulung

--- hashing/src/App.java
+++ hashing/src/App.java
@@ -1,8 +1,8 @@
+import Musterlösung.HasherML;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        byte[] passwort = "Sicher123!".getBytes();
-        
-        Hasher hasher = new Hasher();
+
+        HasherML.HashPasswort();
     }
 }
